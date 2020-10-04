7ea89462981bc4b4fe2ca164e9cd6250a0680f7c
Andres Felipe Cortés Buitrago <andresfe449@hotmail.com>
2020-10-03 22:16:21 -0500

    Delete menu.js

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function name() {
-   $('.menu li:has(ul)').click(function name(e) {
-       e.preventDefault();
-   });
-
-   $('ul').click(function name(s) {
-       s.stopPropagation();
-   });
-});
